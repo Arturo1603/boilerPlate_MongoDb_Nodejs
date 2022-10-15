4e6572e95c0f4deec4d2cfc2b0288ab4c97f8ea2
Arturo1603 <arturo.cq.16@gmail.com>
2022-10-14 22:37:28 -0500

    the last changes

--- src/config/database.js
+++ src/config/database.js
@@ -3,12 +3,18 @@ import { connect } from 'mongoose';
 export default async () => {
     const databases = {
         development: {
-            url:`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-            options:{
+            url: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+            options: {
                 useNewUrlParser: true,
             },
         },
-        production: {},
+        production: {
+            url: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+            options: {
+                seNewUrlParser: true,
+                useUnifiedTopology:true,
+            }
+        },
     };
 
     const config = databases[process.env.NODE_ENV];
@@ -18,5 +24,5 @@ export default async () => {
         if (!error) console.log("MONGODB RUNING");
         else console.log(`MONGODB ERROR => ${error}`);
     });
-    
+
 }
