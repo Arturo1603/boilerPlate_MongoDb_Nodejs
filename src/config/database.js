f9548cadcea5183b4b5b339d1547e076e68574d2
Arturo1603 <arturo.cq.16@gmail.com>
2022-10-14 23:06:08 -0500

    add

--- src/config/database.js
+++ src/config/database.js
@@ -12,7 +12,7 @@ export default async () => {
             url: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
             options: {
                 seNewUrlParser: true,
-                useUnifiedTopology:true,
+                useUnifiedTopology: true,
             }
         },
     };
